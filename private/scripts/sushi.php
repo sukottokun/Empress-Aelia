1d6a0e0f2954f60ba3458c8d2128577788b360ba
Scott Massey <scott.massey@gmail.com>
2021-10-26 05:57:48 +0100

    redo path

--- private/scripts/sushi.php
+++ private/scripts/sushi.php
@@ -1,11 +1,6 @@
 <?php
 
 print("\n==== WP Reset Starting ====\n");
-
-foreach ($_SERVER as $key_name => $key_value) {
-
-    echo $key_name . " = " . $key_value . "\n";
-};
 // Get paths for imports
 $path  = $_SERVER['DOCUMENT_ROOT'] . '/private/data';
 
@@ -21,25 +16,14 @@ $cmd = "unzip ${path}/uploads.zip -d ${files}";
 passthru($cmd);
 
 // Update links
-echo ('Beginning Search and Replace...');
-$host_url = $_SERVER["HTTP_HOST"];
-echo $host_url;
-$new_url = "https://dev-arcadius-product-tokyo.pantheonsite.io";
-$old_url = "https://arcadius-product-uk.lndo.site";
-
 if (!empty($_ENV['PANTHEON_ENVIRONMENT'] && $_ENV['PANTHEON_ENVIRONMENT'] !== 'live') && !empty($_POST['wf_type'] && $_POST['wf_type'] == 'clone_database')) {
-
-    // Get environment contexts
-    $target = $_POST['to_environment'];
-    $source = $_POST['from_environment'];
-    echo $target;
-    echo $source;
 
     // Get domains
     $old_domain = "https://arcadius-product-uk.lndo.site";
-    $new_domain = "https://dev-" . $host_url;
+    $new_domain = "https://dev-" . $_SERVER["WPCLI_URL"];
 
     // Run Search Replace on WPMS sites
+    echo ('Beginning Search and Replace...');
     $cmd = "wp search-replace '{$old_domain}' '{$new_domain}' --precise --recurse-objects --all-tables --verbose --skip-columns=guid";
     passthru($cmd);
 }
